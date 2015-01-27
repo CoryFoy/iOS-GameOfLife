010421944a9e2534b117006dcbcca5f9587fa298
Cory Foy <foyc@Corys-MacBook-Pro.local>
2015-01-26 21:20:53 -0500

    Initial Commit

---
+++ GameOfLife/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GameOfLife
+//
+//  Created by Cory Foy on 1/26/15.
+//  Copyright (c) 2015 Cory Foy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GameOfLife/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  GameOfLife
+//
+
+//  Copyright (c) 2015 Cory Foy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
